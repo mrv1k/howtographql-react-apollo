76b09b3a351894ff7f8c7f40162b7c61f9ea5926
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-11-02 14:01:52 -0400

    Queries: Loading Links

---
+++ src/components/Link.js
@@ -0,0 +1,9 @@
+const Link = ({link}) => (
+  <div>
+    <div>
+      {link.description} ({link.url})
+    </div>
+  </div>
+);
+
+export default Link;
---
+++ src/components/LinkList.js
@@ -0,0 +1,34 @@
+import {useQuery, gql} from '@apollo/client';
+import Link from './Link';
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      id
+      links {
+        id
+        createdAt
+        description
+        url
+      }
+    }
+  }
+`;
+
+const LinkList = () => {
+  const {data} = useQuery(FEED_QUERY);
+
+  return (
+    <div>
+      {data && (
+        <>
+          {data.feed.links.map((link) => (
+            <Link key={link.id} link={link} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default LinkList;
