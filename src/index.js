3efb7f8ff7ae2d7b3db1e581fc4ab674c89edc39
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-11-03 18:08:00 -0400

    Realtime Updates with GraphQL Subscriptions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import {
-  ApolloClient, ApolloProvider, createHttpLink,
-  InMemoryCache
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+  split
 } from '@apollo/client';
 import {setContext} from '@apollo/client/link/context';
+import {WebSocketLink} from '@apollo/client/link/ws';
+import {getMainDefinition} from '@apollo/client/utilities';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './components/App';
 import {AUTH_TOKEN} from './constants';
-import reportWebVitals from './reportWebVitals';
 import './styles/index.css';
 
 const httpLink = createHttpLink({
@@ -25,8 +29,30 @@ const authLink = setContext((_, {headers}) => {
   };
 });
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+const link = split(
+  ({query}) => {
+    const {kind, operation} = getMainDefinition(query);
+    return (
+      kind === 'OperationDefinition' &&
+      operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache(),
 });
 
@@ -41,7 +67,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
